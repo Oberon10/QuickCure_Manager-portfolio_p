cd4f76bc97a276f8ef0b6f14b383cc290ad9749a
SaintAlaye <elitetechydev@gmail.com>
2024-03-21 04:58:59 +0100

    ensure instance folder exists

--- manager/__init__.py
+++ manager/__init__.py
@@ -9,12 +9,13 @@ def create_app(test_config=None):
         DATABASE = os.path.join(app.instance_path, 'manager.sqlite'),
     )
 
-    # load the instance config, if it exist when not testing
     if test_config is None:
+        # load the instance config, if it exist when not testing
         app.config.from_pyfile('config.py', silent= True)
     else:
+        # load the test config if passed in
         app.config.from_mapping(test_config)
-    #
+    # ensure instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
