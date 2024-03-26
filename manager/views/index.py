49f4ad7d58931c7ee0ab852cf27fd6117f852676
James0711 <jengreat94@gmail.com>
2024-03-26 05:55:37 +0100

    created the index blueprint

--- manager/views/index.py
+++ manager/views/index.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("index", __name__, url_prefix="/")
 
+# Create a Home route 
 @bp.route("/")
 def index():
   return render_template("index.html")
