4d9af15389f1cadc6476b69c32181f4d98d30bc9
SaintAlaye <elitetechydev@gmail.com>
2024-03-27 02:42:16 +0100

    fix flask app run issue

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,32 +1,49 @@
 import os
+
 from flask import Flask
 
 
-# create and configure the application
 def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'development',
-        DATABASE = os.path.join(app.instance_path, 'manager.sqlite'),
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "manager.sqlite"),
     )
 
     if test_config is None:
-# load the instance config, if it exist when not testing
-        app.config.from_pyfile('config.py', silent= True)
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
     else:
-# load the test config if passed in
-        app.config.from_mapping(test_config)
-# ensure instance folder exists
+        # load the test config if passed in
+        app.config.update(test_config)
+
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-# intialize  the application
-    from manager import db
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    # register the database commands
+    from manager.model import db
+
     db.init_app(app)
 
-    from . import auth
+    # apply the blueprints to the app
+    from manager.views import auth
+    
     app.register_blueprint(auth.bp)
-   
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
+
     return app
